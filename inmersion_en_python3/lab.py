03f9b0b05762f5d1e556f472d87ecd2a044fcffc
israel <diego.arroyo2462050@gmail.com>
2020-01-10 22:17:53 -0500

    10/01/2020

--- inmersion_en_python3/lab.py
+++ inmersion_en_python3/lab.py
@@ -11,8 +11,38 @@
 #from ficherosDe_clases import peculiaridades
 #peculiaridades.Generadores()
 
-from ficherosDe_clases.peculiaridades import Excepciones
-obj=Excepciones
-obj.exe()
+#from ficherosDe_clases.peculiaridades import Excepciones
+#Excepciones.exe() # just call the class method
+
+#TRABAJANDO CON CLASES
+class Persona():
+	"""sirver para comprender la funcionalidades de la clases en python"""
+	def __init__(self, n="default_name", e="default_age"):
+		self.nombre=n
+		self.edad=e
+
+	def infor(self):
+		print(self.nombre,self.edad)
+
+	def autoCall(self):
+		Persona().infor()
+		x=Persona()
+		y=Persona("nombre_radical","apellido_radical")
+		x.nombre="cambio_radical"
+		x.infor()
+		y.infor()
+		#MUESTRA A LA DOCTSTRING DE LA CLASE A LA QUE PETENECE
+		print(y.__doc__)
+		#MUESTRA A LA CLASE QUE PERTENECE EL OBJETO INSTANCIADO
+		print(y.__class__)
+
+
+def main():
+	Persona().autoCall()
+	
+
+if __name__=="__main__":
+	main()
+
 
 
