4cdff7900d49e7a5583cdabf150d9bcb2891f940
israel <diego.arroyo2462050@gmail.com>
2020-04-29 18:00:42 -0500

    ...

--- ar_python/python_ar.py
+++ ar_python/python_ar.py
@@ -14,9 +14,32 @@ def curious_fact_1():
 			lista.append(n)
 		print(lista,end="\n")
 
+def curious_fact_2():
+	"""/
+	lambdas: son funciones anonimas, practicas (hasta ahora)
+	"""
+	yoin = lambda sep,cad : sep.join(cad) #declaracion de la funcion lambda
+
+	x=yoin('/',["home","israel","DEV"]) #mandando parametros a la funcion
+	print(x)
+
+def curious_fact_3():
+	"""/
+	desempaquetando argumentos o parametros en una funcion
+	Argumentos nombrados
+	"""
+	def funtion_1(nombre,apellido,ci):
+		print("Hola {} {}, tu CI es: {}".format(nombre,apellido,ci))
+
+	arg_1=["Diego","Israel", 91991]
+	arg_2={"nombre":"Jhon","apellido":"Alejandro","ci":"091230"}
+	funtion_1(*arg_1)
+	funtion_1(**arg_2)
+
 class for__(object):
 	"""\
 	Aqui estan todos los datos curiosos respecto al bucle FOR
+	DC = Dato Curioso
 	"""
 	def dc_1():
 		"""DC: for con incremento diferente de 1 (lo usual) """
@@ -48,21 +71,59 @@ class for__(object):
 		     print("Encontré un número", num)
 
 
-	def dc_3():
+	def dc_4():
 		"""Bucle FOR mas sentencia enumerate"""
 		lgdp=["Python","C++","Java",["HTML","CSS","JS"]]
 		for i,lg in enumerate(lgdp):
 			print(i,lg)
 
+	def dc_5():
+		"""\
+		Este ejercicio muestra la complejidad de las listas por compresion
+		anidadas, cada funcion estas mas simplificada que la otra pero todas
+		hacen el mismo trabajo (calcular la traspuesta de una matriz)
+		"""
+		def traspuesta_metodo_1(vec):
+			traspuesta = []
+
+			for i in range(3):
+				fila_traspuesta = []
+				for fila in vec:
+					fila_traspuesta.append(fila[i])
+
+				traspuesta.append(fila_traspuesta)
+
+			return (traspuesta)
+
+		def traspuesta_metodo_2(vec):
+			trs = []
+			for i in range(3):
+				trs.append([x[i] for x in vec])
+			return trs
+
+		def traspuesta_metodo_3(vec):
+			traspuesta = []
+			traspuesta = [[fila[x]  for fila in vec] for x in range(3)]
+			return (traspuesta)
+
+		vec = [[1,2,3], [4,5,6], [7,8,9]]
+		print(traspuesta_metodo_1(vec))
+		print(traspuesta_metodo_2(vec))
+		print(traspuesta_metodo_3(vec))
+
+
+
 def main():
 	#print(curious_fact_1.__doc__) #muestra el doc de la funcion, si es que lo tiene
 	#print(for__.dc_1.__doc__) #muestra la documentacion de una funcion interna
-	for__.dc_3()
+	#for__.dc_5()
+	#curious_fact_3()
 
-	
 
-	
 
+	l = [1,2,3,4,5,6]
+	del l[1:4]
+	print(l)
 
 if __name__ == "__main__":
 	main()
